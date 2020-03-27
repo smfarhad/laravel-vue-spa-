bbee7cf17a3cfd5ad74f57f719a277faf08fb629
smfarhad <farhad1556@gmail.com>
2020-03-28 04:54:45 +0600

    Define Relationships

--- app/Model/Reply.php
+++ app/Model/Reply.php
@@ -1,10 +1,22 @@
 <?php
 
 namespace App\Model;
+use App\Question;
+use App\User;
+use App\Like;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Reply extends Model
 {
-    //
+    public function question(){
+        return $this->belongsTo(Question::class);
+    }
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+    public function like(){
+        return $this->hasMany(Like::class);
+    }
 }
+ 
